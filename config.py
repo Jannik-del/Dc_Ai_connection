63cfd3309c8da8affbe5443e42fd13edf54b14d9
Jannik <147308124+Jannik-del@users.noreply.github.com>
2025-03-15 23:38:12 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 BOT_TOKEN = "YOUR_DC_BOT_TOKEN"
-GUILD_ID = "THE_SERVER_ID"
+GUILD_ID = THE_SERVER_ID
 OPENROUTER_CHANNEL_ID = 123456789 #the channel id where the bot should listen and respond
 OPENROUTER_API_KEY = "YOUR_OPENROUTER_API_KEY_HERE" #https://openrouter.ai
